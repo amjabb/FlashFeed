cf532f9b8f7cc77bcba475784ff0c1fc9584ae44
sirial <amirjabbari@Amirs-MacBook-Pro.local>
2016-11-21 01:55:32 -0800

    First commit

---
+++ PopOverViewController.swift
@@ -0,0 +1,43 @@
+//
+//  PopOverViewController.swift
+//  NoName
+//
+//  Created by Amir Jabbari on 11/15/16.
+//  Copyright © 2016 Amir Jabbari. All rights reserved.
+//
+
+import UIKit
+
+class PopOverViewController: UIViewController {
+
+    
+    @IBAction func postTextButton(_ sender: Any) {
+        performSegue(withIdentifier: "showPostView", sender: sender)
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
